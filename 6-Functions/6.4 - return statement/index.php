8f6afc69a6319f947b35948d683a684d6bfad474
LorenzovandenDungen <Lorenzo.vandendungen@student.rocva.nl>
2021-12-14 13:54:13 +0100

    6.4 Lorenzo

--- 6-Functions/6.4 - return statement/index.php
+++ 6-Functions/6.4 - return statement/index.php
@@ -1,3 +1,13 @@
 <?php
-    
+    function geefTienProcent(){
+        $koopprijs = 75;
+        $korting = 10;
+        return $koopprijs = $koopprijs * ((100-$korting) / 100);
+    }
+
+    function zetAllesgroot(){
+        echo '<h1>' . geefTienProcent() . '</h1>';
+    }
+
+    zetAllesgroot();
 ?>
