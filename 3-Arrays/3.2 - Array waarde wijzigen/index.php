ea8f4527b3672f8d9b2c191b2160d4325c00dfaa
LorenzovandenDungen <Lorenzo.vandendungen@student.rocva.nl>
2022-01-26 10:27:19 +0100

    verbeterd

--- 3-Arrays/3.2 - Array waarde wijzigen/index.php
+++ 3-Arrays/3.2 - Array waarde wijzigen/index.php
@@ -1,6 +1,13 @@
 <?php
-$MyDogNames = ['Tijger ', 'Lotje ', 'Mickey '];
+$MyDogNames = ['<li>Tijger</li> ', '<li>Lotje</li> ', '<li>Mickey</li> '];
 echo $MyDogNames[0];
-echo $MyDogNames[1] = 'Bart ';
+echo $MyDogNames[1];
 echo $MyDogNames[2];
+
+$mydognames[1] = '<li>Bart</li>';
+$mydognames[0] = '<li>Junior</li>';
+$mydognames[2] = '<li>bas</li>';
+echo $mydognames[1];
+echo $mydognames[0];
+echo $mydognames[2];
 ?>
